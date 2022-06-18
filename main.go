b04450d1f4d60a82fabd8d2190c9ca883670272b
Shu Kutsuzawa <cappyzawa@gmail.com>
2022-06-18 13:47:11 +0900

    enable to distinguish step

--- main.go
+++ main.go
@@ -8,9 +8,42 @@ import (
 	"github.com/concourse/concourse/atc"
 )
 
+func stepSchema(schema *jsonschema.Schema) {
+	stepDef := schema.Definitions["Step"]
+
+	// thx: https://gist.github.com/4662752c4af75b3220d94b657683d090
+	// https://concourse-ci.org/steps.html#schema.step
+	stepConfigs := []atc.StepConfig{
+		&atc.GetStep{},
+		&atc.PutStep{},
+		&atc.TaskStep{},
+		&atc.SetPipelineStep{},
+		&atc.LoadVarStep{},
+		&atc.InParallelStep{},
+		&atc.DoStep{},
+		&atc.TryStep{},
+	}
+
+	for _, s := range stepConfigs {
+		stepSchema := jsonschema.Reflect(s)
+		stepDef.AnyOf = append(stepDef.AnyOf, stepSchema.Type)
+		for subName, subDef := range stepSchema.Definitions {
+			if _, present := schema.Definitions[subName]; !present {
+				schema.Definitions[subName] = subDef
+			}
+		}
+	}
+	stepDef.Required = nil
+	schema.Definitions["Step"].Properties = nil
+	schema.Definitions["Step"].Type = ""
+	schema.Definitions["Step"].AdditionalProperties = nil
+}
+
 func main() {
 	var pipelineConfig atc.Config
-	reflected, err := jsonschema.Reflect(&pipelineConfig).MarshalJSON()
+	schema := jsonschema.Reflect(&pipelineConfig)
+	stepSchema(schema)
+	reflected, err := schema.MarshalJSON()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
